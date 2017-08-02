9b5a3c660c07b086e00b9e484f736244bd7b3afe
Unknown <red.john@gmx.de>
2017-08-02 16:18:50 +0200

    added "not an md5 hash" to change background red

--- js/not_found_handler.js
+++ js/not_found_handler.js
@@ -4,5 +4,9 @@ setInterval(function(){
           $(this).css('background-color','#f33');
           $(this).css('font-weight','bold');
       }
+      if ($(this).text() == 'not an md5 hash') {
+          $(this).css('background-color','#f33');
+          $(this).css('font-weight','bold');
+      }
   });
 }, 500);
